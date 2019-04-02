dee65a79ecfe66c6e57b5bf3e4b3e4b14bed07ab
simkas <simkas12@gmail.com>
2019-04-02 19:24:15 +0300

    unhide som 3cb, gloves

---
+++ 3cb_unhidden/config.cpp
@@ -0,0 +1,563 @@
+class CfgPatches
+{
+	class 3cb_unhidden
+	{
+		units[]={};
+		weapons[]={};
+		magazines[]={};
+		requiredVersion=1;
+		requiredAddons[]=
+		{
+			"UK3CB_Factions_CHC_B",
+			"UK3CB_Factions_CCM_B",
+			"UK3CB_Factions_TKC_B",
+			"UK3CB_Factions_ANP_B",
+			"UK3CB_Factions_TKM_B",
+			"UK3CB_Factions_TKP_B",
+			"UK3CB_Factions_TKA_B",
+			"UK3CB_Factions_TKM_I",
+			"UK3CB_Factions_TKC_I",
+			"UK3CB_Factions_TKM_O",
+			"UK3CB_Factions_TKC_O",
+			"UK3CB_Factions_CCM_O",
+			"UK3CB_Factions_CHC_O"
+		};
+	};
+};
+class CfgWeapons
+{
+	class UK3CB_TKM_O_U_01;
+	class UK3CB_TKM_O_U_01_B: UK3CB_TKM_O_U_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_U_01_C: UK3CB_TKM_O_U_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_U_01_D: UK3CB_TKM_O_U_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_U_03;
+	class UK3CB_TKM_O_U_03_B: UK3CB_TKM_O_U_03
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_U_03_C: UK3CB_TKM_O_U_03
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_U_04;
+	class UK3CB_TKM_O_U_04_B: UK3CB_TKM_O_U_04
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_U_04_C: UK3CB_TKM_O_U_04
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_U_05;
+	class UK3CB_TKM_O_U_05_B: UK3CB_TKM_O_U_05
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_U_05_C: UK3CB_TKM_O_U_05
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_U_06;
+	class UK3CB_TKM_O_U_06_B: UK3CB_TKM_O_U_06
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_U_06_C: UK3CB_TKM_O_U_06
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_U_01;
+	class UK3CB_TKM_B_U_01_B: UK3CB_TKM_B_U_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_U_01_C: UK3CB_TKM_B_U_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_U_03;
+	class UK3CB_TKM_B_U_03_B: UK3CB_TKM_B_U_03
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_U_03_C: UK3CB_TKM_B_U_03
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_U_04;
+	class UK3CB_TKM_B_U_04_B: UK3CB_TKM_B_U_04
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_U_04_C: UK3CB_TKM_B_U_04
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_U_05;
+	class UK3CB_TKM_B_U_05_B: UK3CB_TKM_B_U_05
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_U_05_C: UK3CB_TKM_B_U_05
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_U_06;
+	class UK3CB_TKM_B_U_06_B: UK3CB_TKM_B_U_06
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_U_06_C: UK3CB_TKM_B_U_06
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_I_U_01;
+	class UK3CB_TKM_I_U_01_B: UK3CB_TKM_I_U_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_I_U_01_C: UK3CB_TKM_I_U_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_I_U_03;
+	class UK3CB_TKM_I_U_03_B: UK3CB_TKM_I_U_03
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_I_U_03_C: UK3CB_TKM_I_U_03
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_I_U_04;
+	class UK3CB_TKM_I_U_04_B: UK3CB_TKM_I_U_04
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_I_U_04_C: UK3CB_TKM_I_U_04
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_I_U_05;
+	class UK3CB_TKM_I_U_05_B: UK3CB_TKM_I_U_05
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_I_U_05_C: UK3CB_TKM_I_U_05
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_I_U_06;
+	class UK3CB_TKM_I_U_06_B: UK3CB_TKM_I_U_06
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_I_U_06_C: UK3CB_TKM_I_U_06
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_H_Turban_01_1;
+	class UK3CB_TKM_B_H_Turban_01_1;
+	class UK3CB_TKM_B_H_Turban_01_2: UK3CB_TKM_B_H_Turban_01_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_H_Turban_01_3: UK3CB_TKM_B_H_Turban_01_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_H_Turban_01_2: UK3CB_TKM_O_H_Turban_01_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_H_Turban_01_3: UK3CB_TKM_O_H_Turban_01_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_H_Turban_02_1;
+	class UK3CB_TKM_B_H_Turban_02_2: UK3CB_TKM_B_H_Turban_02_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_B_H_Turban_02_3: UK3CB_TKM_B_H_Turban_02_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_H_Turban_02_1;
+	class UK3CB_TKM_O_H_Turban_02_2: UK3CB_TKM_O_H_Turban_02_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_H_Turban_02_3: UK3CB_TKM_O_H_Turban_02_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_H_Turban_03_1;
+	class UK3CB_TKM_O_H_Turban_03_2: UK3CB_TKM_O_H_Turban_03_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_H_Turban_03_3: UK3CB_TKM_O_H_Turban_03_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_H_Turban_04_1;
+	class UK3CB_TKM_O_H_Turban_04_2: UK3CB_TKM_O_H_Turban_04_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_H_Turban_04_3: UK3CB_TKM_O_H_Turban_04_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_H_Turban_05_1;
+	class UK3CB_TKM_O_H_Turban_05_2: UK3CB_TKM_O_H_Turban_05_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKM_O_H_Turban_05_3: UK3CB_TKM_O_H_Turban_05_1
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_ACTIVIST_01;
+	class UK3CB_CHC_C_U_ACTIVIST_02: UK3CB_CHC_C_U_ACTIVIST_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_ACTIVIST_03: UK3CB_CHC_C_U_ACTIVIST_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_ACTIVIST_04: UK3CB_CHC_C_U_ACTIVIST_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_CIT_01;
+	class UK3CB_CHC_C_U_CIT_02: UK3CB_CHC_C_U_CIT_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_CIT_03: UK3CB_CHC_C_U_CIT_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_CIT_04: UK3CB_CHC_C_U_CIT_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_COACH_01;
+	class UK3CB_CHC_C_U_COACH_02: UK3CB_CHC_C_U_COACH_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_COACH_03: UK3CB_CHC_C_U_COACH_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_COACH_04: UK3CB_CHC_C_U_COACH_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_COACH_05: UK3CB_CHC_C_U_COACH_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_FUNC_01;
+	class UK3CB_CHC_C_U_FUNC_02: UK3CB_CHC_C_U_FUNC_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_HIKER_01;
+	class UK3CB_CHC_C_U_HIKER_02: UK3CB_CHC_C_U_HIKER_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_HIKER_03: UK3CB_CHC_C_U_HIKER_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_HIKER_04: UK3CB_CHC_C_U_HIKER_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_Pilot_01;
+	class UK3CB_CHC_C_U_Pilot_02: UK3CB_CHC_C_U_Pilot_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_Pilot_03: UK3CB_CHC_C_U_Pilot_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_POLITIC_01;
+	class UK3CB_CHC_C_U_POLITIC_02: UK3CB_CHC_C_U_POLITIC_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_POLITIC_03: UK3CB_CHC_C_U_POLITIC_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_PRIEST_01;
+	class UK3CB_CHC_C_U_PRIEST_02: UK3CB_CHC_C_U_PRIEST_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_PRIEST_03: UK3CB_CHC_C_U_PRIEST_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_PROF_01;
+	class UK3CB_CHC_C_U_PROF_02: UK3CB_CHC_C_U_PROF_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_PROF_03: UK3CB_CHC_C_U_PROF_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_PROF_04: UK3CB_CHC_C_U_PROF_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_VILL_01;
+	class UK3CB_CHC_C_U_VILL_02: UK3CB_CHC_C_U_VILL_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_VILL_03: UK3CB_CHC_C_U_VILL_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_VILL_04: UK3CB_CHC_C_U_VILL_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_WOOD_01;
+	class UK3CB_CHC_C_U_WOOD_02: UK3CB_CHC_C_U_WOOD_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_WOOD_03: UK3CB_CHC_C_U_WOOD_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_WOOD_04: UK3CB_CHC_C_U_WOOD_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_WORK_01;
+	class UK3CB_CHC_C_U_WORK_02: UK3CB_CHC_C_U_WORK_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_WORK_03: UK3CB_CHC_C_U_WORK_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_CHC_C_U_WORK_04: UK3CB_CHC_C_U_WORK_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_C_U_Doc;
+	class UK3CB_TKC_C_U_Doc_B: UK3CB_TKC_C_U_Doc
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_C_U_Doc_C: UK3CB_TKC_C_U_Doc
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_C_U_Overall;
+	class UK3CB_TKC_C_U_Overall_B: UK3CB_TKC_C_U_Overall
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_C_U_Overall_C: UK3CB_TKC_C_U_Overall
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_C_U_Overall_D: UK3CB_TKC_C_U_Overall
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_C_U_Overall_E: UK3CB_TKC_C_U_Overall
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_C_U_Pilot_A;
+	class UK3CB_TKC_C_U_Pilot_B: UK3CB_TKC_C_U_Pilot_A
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_01;
+	class UK3CB_TKC_B_U_01_B: UK3CB_TKC_B_U_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_01_C: UK3CB_TKC_B_U_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_01_D: UK3CB_TKC_B_U_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_02;
+	class UK3CB_TKC_B_U_02_B: UK3CB_TKC_B_U_02
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_02_C: UK3CB_TKC_B_U_02
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_02_D: UK3CB_TKC_B_U_02
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_03;
+	class UK3CB_TKC_B_U_03_B: UK3CB_TKC_B_U_03
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_03_C: UK3CB_TKC_B_U_03
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_03_D: UK3CB_TKC_B_U_03
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_03_E: UK3CB_TKC_B_U_03
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_06;
+	class UK3CB_TKC_B_U_06_B: UK3CB_TKC_B_U_06
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_06_C: UK3CB_TKC_B_U_06
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_06_D: UK3CB_TKC_B_U_06
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_TKC_B_U_06_E: UK3CB_TKC_B_U_06
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Profiteer_Cap_01;
+	class UK3CB_H_Profiteer_Cap_02: UK3CB_H_Profiteer_Cap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Profiteer_Cap_03: UK3CB_H_Profiteer_Cap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Profiteer_Cap_04: UK3CB_H_Profiteer_Cap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Ushanka_Cap_01;
+	class UK3CB_H_Ushanka_Cap_02: UK3CB_H_Ushanka_Cap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Ushanka_Cap_03: UK3CB_H_Ushanka_Cap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Ushanka_Cap_04: UK3CB_H_Ushanka_Cap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Villager_Cap_01;
+	class UK3CB_H_Villager_Cap_02: UK3CB_H_Villager_Cap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Villager_Cap_03: UK3CB_H_Villager_Cap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Villager_Cap_04: UK3CB_H_Villager_Cap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Worker_Cap_01;
+	class UK3CB_H_Worker_Cap_02: UK3CB_H_Worker_Cap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Worker_Cap_03: UK3CB_H_Worker_Cap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_H_Worker_Cap_04: UK3CB_H_Worker_Cap_01
+	{
+		scopeArsenal=2;
+	};
+};
+class CfgGlasses
+{
+	class UK3CB_G_Neck_Shemag;
+	class UK3CB_G_Neck_Shemag_KL_blk: UK3CB_G_Neck_Shemag
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_G_Neck_Shemag_KL_tan: UK3CB_G_Neck_Shemag
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_G_Neck_Shemag_KLR_blk: UK3CB_G_Neck_Shemag
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_G_Neck_Shemag_KLR_tan: UK3CB_G_Neck_Shemag
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_G_Neck_Shemag_KR_blk: UK3CB_G_Neck_Shemag
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_G_Neck_Shemag_KR_tan: UK3CB_G_Neck_Shemag
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_G_Face_Wrap_01;
+	class UK3CB_G_Face_Wrap_02: UK3CB_G_Face_Wrap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_G_Face_Wrap_03: UK3CB_G_Face_Wrap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_G_Face_Wrap_04: UK3CB_G_Face_Wrap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_G_Face_Wrap_05: UK3CB_G_Face_Wrap_01
+	{
+		scopeArsenal=2;
+	};
+	class UK3CB_G_Face_Wrap_06: UK3CB_G_Face_Wrap_01
+	{
+		scopeArsenal=2;
+	};
+};
