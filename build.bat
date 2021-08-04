fe7863473f7d2a26258606433f0b776dae206ad2
diwako <diwako@gmail.com>
2021-08-04 08:40:19 +0200

    add warnings are errors to build script

--- build.bat
+++ build.bat
@@ -5,7 +5,7 @@ for /d %%G in ("FP_*") do (
 	echo.
 	echo Building %%G...
 	echo.
-	makepbo -p -@=%%G %%G build/%%G || goto error
+	makepbo -W -p -@=%%G %%G build/%%G || goto error
 )
 
 makepbo -p -W -u -@=sim_extra_gear sim_extra_gear build/sim_extra_gear || goto error
